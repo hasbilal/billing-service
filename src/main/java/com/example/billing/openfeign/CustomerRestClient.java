d726a5b29fe96b8c3f88d88d38c71f7394ee8e48
hasbilal <hasbilal92@gmail.com>
2021-09-24 13:19:27 +0200

    Synch communication

---
+++ src/main/java/com/example/billing/openfeign/CustomerRestClient.java
@@ -0,0 +1,17 @@
+package com.example.billing.openfeign;
+
+import com.example.billing.entities.Customer;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient("CUSTOMER-SERVICE")
+public interface CustomerRestClient {
+    @GetMapping(path = "/api/customers/{id}")
+    Customer getCustomer(@PathVariable(name = "id") String id);
+
+    @GetMapping(path = "/api/customers")
+    List<Customer> allCustomers();
+}
