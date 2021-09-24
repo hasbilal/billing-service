459c3066594c9dbf7e78a06fbbf07457e9c168f5
hasbilal <hasbilal92@gmail.com>
2021-09-24 12:58:05 +0200

    Invoice Bean

---
+++ src/main/java/com/example/billing/entities/Invoice.java
@@ -0,0 +1,28 @@
+package com.example.billing.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Invoice {
+    @Id
+    private String id;
+    private Date date;
+    private BigDecimal amount;
+    private String clientId;
+
+    @Transient
+    private Customer customer;
+
+
+}
