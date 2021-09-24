3896eeb7ea04ed32f5bf63bab857b5976c60d0d4
hasbilal <hasbilal92@gmail.com>
2021-09-24 13:03:23 +0200

    Invoice repository

--- src/main/java/com/example/billing/repositories/InvoiceRepository.java
+++ src/main/java/com/example/billing/repositories/InvoiceRepository.java
@@ -1,7 +1,11 @@
 package com.example.billing.repositories;
 
+import com.example.billing.entities.Customer;
 import com.example.billing.entities.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InvoiceRepository extends JpaRepository<Invoice,String> {
+        List<Invoice> findByClientId(String clientId);
 }
